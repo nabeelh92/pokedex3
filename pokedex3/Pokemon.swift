f84afbf30f5c443f1789e2d91e10d015d8474fc9
nabeelh92 <nabeelhuq@gmail.com>
2017-08-01 22:05:49 -0700

    created a pokemon Class

---
+++ pokedex3/Pokemon.swift
@@ -0,0 +1,33 @@
+//
+//  Pokemon.swift
+//  pokedex3
+//
+//  Created by Nabeel Huq on 2017-08-01.
+//  Copyright © 2017 Nabeel Huq. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        
+        return _name
+    }
+    
+    var pokedexID: Int {
+        
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        
+        self._name = name
+        self._pokedexId = pokedexId
+    
+    }
+    
+}
