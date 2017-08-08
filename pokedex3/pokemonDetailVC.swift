424ede310d3ff2566469b921bd57e1fb284c8b4a
nabeelh92 <nabeelhuq@gmail.com>
2017-08-07 19:58:51 -0700

    added outlets for all descriptionVC labels and updated pokemon model class with new properties

--- pokedex3/pokemonDetailVC.swift
+++ pokedex3/pokemonDetailVC.swift
@@ -12,13 +12,29 @@ class pokemonDetailVC: UIViewController {
 
     var pokemon: Pokemon!
     
-    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        nameLabel.text = pokemon.name
+        nameLbl.text = pokemon.name
 
     }
 
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
 }
